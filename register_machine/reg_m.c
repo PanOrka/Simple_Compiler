bb059240bf9d2dfc62d624f16c5938cb71902d33
pan_orka <ltgaming.reterri@gmail.com>
2020-12-12 01:09:22 +0100

    register_machine: +refactor, +register_drop

--- register_machine/reg_m.c
+++ register_machine/reg_m.c
@@ -7,38 +7,65 @@ reg_set reg_m_create() {
     for (int32_t i=0; i<REG_SIZE; ++i) {
         regs.r[i].addr = ADDR_UNDEF;
         regs.r[i].id = reg_id_arr[i];
+        regs.r[i].flags = REG_NO_FLAGS;
     }
 
     return regs;
 }
 
-void reg_m_sort(uint32_t idx, reg_set *r_set) {
-    if (idx < REG_SIZE-1) {
-        reg temp = r_set->r[idx];
+#ifndef REG_M_SORT_UP
+#define REG_M_SORT_UP 1
+#endif
 
+#ifndef REG_M_SORT_DOWN
+#define REG_M_SORT_DOWN -1
+#endif
+
+void reg_m_sort(reg_set *r_set, uint32_t idx, int32_t type) {
+    reg temp = r_set->r[idx];
+
+    if (type == REG_M_SORT_UP) {
         for (uint32_t i=idx; i<REG_SIZE-1; ++i) {
             r_set->r[i] = r_set->r[i+1];
         }
 
         r_set->r[REG_SIZE-1] = temp;
+    } else {
+        for (uint32_t i=idx; i>0; --i) {
+            r_set->r[i] = r_set->r[i-1];
+        }
+
+        r_set->r[0] = temp;
     }
 }
 
-reg_allocator reg_m_get(uint64_t addr, reg_set *r_set) {
+reg_allocator reg_m_get(reg_set *r_set, addr_t addr) {
     for (int32_t i=0; i<REG_SIZE; ++i) {
         if (r_set->r[i].addr == addr) {
-            reg_m_sort(i, r_set);
+            reg_m_sort(r_set, i, REG_M_SORT_UP);
 
             return (reg_allocator){&(r_set->r[REG_SIZE-1]), i, true};
         }
     }
 
-    reg_m_sort(0, r_set);
+    reg_m_sort(r_set, 0, REG_M_SORT_UP);
     return (reg_allocator){&(r_set->r[REG_SIZE-1]), 0, false};
 }
 
 reg_allocator reg_m_LRU(reg_set *r_set) {
-    reg_m_sort(0, r_set);
+    reg_m_sort(r_set, 0, REG_M_SORT_UP);
 
     return (reg_allocator){&(r_set->r[REG_SIZE-1]), 0, false};
+}
+
+void reg_m_drop_addr(reg_set *r_set, addr_t addr) {
+    for (int32_t i=0; i<REG_SIZE; ++i) {
+        if (r_set->r[i].addr == addr) {
+            r_set->r[i].addr = ADDR_UNDEF;
+            r_set->r[i].flags = REG_NO_FLAGS;
+            reg_m_sort(r_set, i, REG_M_SORT_DOWN);
+
+            return;
+        }
+    }
 }
--- register_machine/reg_m.h
+++ register_machine/reg_m.h
@@ -10,9 +10,18 @@
 
 #include "../definitions.h"
 
+#ifndef REG_NO_FLAGS
+#define REG_NO_FLAGS 0b00000000
+#endif
+
+#ifndef REG_MODIFIED
+#define REG_MODIFIED 0b10000000
+#endif
+
 typedef struct {
     addr_t addr;
     char id;
+    uint8_t flags;
 } reg;
 
 typedef struct {
@@ -40,10 +49,10 @@ reg_set reg_m_create();
  * If addr exists in reg_set then gives existing otherwise chooses Least Used Register
  * Returned register becomes 1st
  * 
- * RETURN: reg_allocator: ptr to register, old idx of register and was_allocated flag
+ * RETURN: reg_allocator: ptr to register set, old idx of register and was_allocated flag
  * 
 */
-reg_allocator reg_m_get(addr_t addr, reg_set *r_set);
+reg_allocator reg_m_get(reg_set *r_set, addr_t addr);
 
 /**
  * 
@@ -51,9 +60,17 @@ reg_allocator reg_m_get(addr_t addr, reg_set *r_set);
  * Use it wisely for temporary objects that aren't addressed, best to use for single fast operations
  * Returned register becomes 1st
  * 
- * RETURN: reg_allocator: ptr to register, old idx of register and was_allocated flag
+ * RETURN: reg_allocator: ptr to register set, old idx of register and was_allocated flag
  * 
 */
 reg_allocator reg_m_LRU(reg_set *r_set);
+
+/**
+ * 
+ * Drops register with given address
+ * Used mainly to drop iterators and FORLOOP variables
+ * 
+*/
+void reg_m_drop_addr(reg_set *r_set, addr_t addr);
 
 #endif
