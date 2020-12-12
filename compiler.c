a5bc37bb8427fbac4277599262b76d1e5e33395f
pan_orka <ltgaming.reterri@gmail.com>
2020-12-12 14:24:02 +0100

    full prelexer and main()

--- compiler.c
+++ compiler.c
@@ -1,5 +1,19 @@
 #include "prelexer.h"
 
-int main() {
-    prelex(NULL, NULL);
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        FILE *in = fopen(argv[1], "r");
+
+        char const * const temp_out = "prelexer_temp_out";
+        FILE *prelex_out = fopen(temp_out, "w+");
+
+        prelex(fopen("test.txt", "r"), prelex_out);
+        fclose(in);
+        remove(temp_out);
+    } else {
+        fprintf(stderr, "WRONG AMOUNT OF INPUT ARGUMENTS!\n");
+        exit(EXIT_FAILURE);
+    }
 }
