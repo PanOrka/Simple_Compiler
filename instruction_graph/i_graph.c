9adb7a22d5b43ad53f4fecd767dc758e981d3a78
PanOrka <ltgaming.reterri@gmail.com>
2020-12-26 14:16:51 +0100

    loop local iterators with expressions

--- instruction_graph/i_graph.c
+++ instruction_graph/i_graph.c
@@ -7,7 +7,7 @@
 static i_graph *start = NULL;
 static i_graph *end = NULL;
 
-static void add_to_list(void *payload, instruction_type i_type) {
+void add_to_list(void *payload, instruction_type i_type) {
     i_graph *new_element = malloc(sizeof(i_graph));
     if (new_element) {
         new_element->payload = payload;
@@ -30,79 +30,6 @@ static void add_to_list(void *payload, instruction_type i_type) {
         fprintf(stderr, "[I_GRAPH]: Couldn't allocate memory for I_Graph node!\n");
         exit(EXIT_FAILURE);
     }
-}
-
-static void add_EXPR(expression_t *expr) {
-    if (expr->var_1[0].var->flags & SYMBOL_IS_ARRAY) {
-        if (!(expr->mask & ASSIGN_SYM2_NUM)) {
-            if (!(expr->var_2[0].var->flags & SYMBOL_INITIALIZED)) {
-                fprintf(stderr, "[I_GRAPH]: Symbol %s is not initialized!\n", expr->var_2[0].var->identifier);
-                fprintf(stderr, "[EXPRESSION]:\n");
-                print_expression(expr, stderr);
-                exit(EXIT_FAILURE);
-            } else {
-                expr->var_2[0].addr = expr->var_2[0].var->addr[0];
-                expr->addr_mask |= ASSIGN_SYM2_ADDR;
-            }
-        }
-    } else {
-        //
-        // LOOP ITERATORS CANNOT BE MODIFIED SO HANDLE IT HERE!!!!
-        //
-        expr->var_1[0].var->flags |= SYMBOL_INITIALIZED;
-    }
-
-    if (!(expr->mask & LEFT_SYM1_NUM)) {
-        if (expr->var_1[1].var->flags & SYMBOL_IS_ARRAY) {
-            if (!(expr->mask & LEFT_SYM2_NUM)) {
-                if (!(expr->var_2[1].var->flags & SYMBOL_INITIALIZED)) {
-                    fprintf(stderr, "[I_GRAPH]: Symbol %s is not initialized!\n", expr->var_2[1].var->identifier);
-                    fprintf(stderr, "[EXPRESSION]:\n");
-                    print_expression(expr, stderr);
-                    exit(EXIT_FAILURE);
-                } else {
-                    expr->var_2[1].addr = expr->var_2[1].var->addr[0];
-                    expr->addr_mask |= LEFT_SYM2_ADDR;
-                }
-            }
-        } else if (!(expr->var_1[1].var->flags & SYMBOL_INITIALIZED)) {
-            fprintf(stderr, "[I_GRAPH]: Symbol %s is not initialized!\n", expr->var_1[1].var->identifier);
-            fprintf(stderr, "[EXPRESSION]:\n");
-            print_expression(expr, stderr);
-            exit(EXIT_FAILURE);
-        } else {
-            expr->var_1[1].addr = expr->var_1[1].var->addr[0];
-            expr->addr_mask |= LEFT_SYM1_ADDR;
-        }
-    }
-
-    if (expr->type != expr_VALUE) {
-        if (!(expr->mask & RIGHT_SYM1_NUM)) {
-            if (expr->var_1[2].var->flags & SYMBOL_IS_ARRAY) {
-                if (!(expr->mask & RIGHT_SYM2_NUM)) {
-                    if (!(expr->var_2[2].var->flags & SYMBOL_INITIALIZED)) {
-                        fprintf(stderr, "[I_GRAPH]: Symbol %s is not initialized!\n", expr->var_2[2].var->identifier);
-                        fprintf(stderr, "[EXPRESSION]:\n");
-                        print_expression(expr, stderr);
-                        exit(EXIT_FAILURE);
-                    } else {
-                        expr->var_2[2].addr = expr->var_2[2].var->addr[0];
-                        expr->addr_mask |= RIGHT_SYM2_ADDR;
-                    }
-                }
-            } else if (!(expr->var_1[2].var->flags & SYMBOL_INITIALIZED)) {
-                fprintf(stderr, "[I_GRAPH]: Symbol %s is not initialized!\n", expr->var_1[2].var->identifier);
-                fprintf(stderr, "[EXPRESSION]:\n");
-                print_expression(expr, stderr);
-                exit(EXIT_FAILURE);
-            } else {
-                expr->var_1[2].addr = expr->var_1[2].var->addr[0];
-                expr->addr_mask |= RIGHT_SYM1_ADDR;
-            }
-        }
-    }
-
-    add_to_list(expr, EXPR);
 }
 
 void i_graph_add_instruction(void *payload, instruction_type i_type) {
