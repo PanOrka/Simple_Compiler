d15c5e77ec307566dd113502bea0c4e325f64fcc
PanOrka <ltgaming.reterri@gmail.com>
2020-12-26 15:10:48 +0100

    NULL safety added

--- parser_func/loops.c
+++ parser_func/loops.c
@@ -11,7 +11,9 @@ void loop_get(char *iter, for_loop_t *loop) {
     symbol_table *s_table = get_symbol_table();
     symbol *hide = symbol_table_find_id(s_table, iter, true, SYMBOL_NO_HIDDEN);
 
-    hide->flags &= ~SYMBOL_NO_HIDDEN;
+    if (hide) {
+        hide->flags &= ~SYMBOL_NO_HIDDEN;
+    }
     loop->iterator.var = symbol_table_add(s_table, iter, (add_info){ .hide = hide }, 1, SYMBOL_IS_ITER | SYMBOL_INITIALIZED);
 
     expression_get(&(loop->range_vars));
