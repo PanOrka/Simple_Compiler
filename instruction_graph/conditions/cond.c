d49fc2fb2b7a11163b1f4465ae4368e4642c7eda
pan_orka <ltgaming.reterri@gmail.com>
2021-01-09 22:58:13 +0100

    conds ready

--- instruction_graph/conditions/cond.c
+++ instruction_graph/conditions/cond.c
@@ -42,10 +42,40 @@ reg * cond_val_from_vals(val src_1, val src_2, expr_type type) {
 
     switch (type) {
         case cond_IS_EQUAL: // x == y
-            return NULL;
+            if (y->flags & REG_MODIFIED) {
+                stack_ptr_generate(y->addr);
+                STORE(y, &(r_set->stack_ptr));
+                y->flags &= ~REG_MODIFIED;
+            }
+            reg *temp = oper_get_reg_for_variable(TEMP_ADDR_3).r;
+            oper_reg_swap(temp, x);
+            SUB(temp, y);
+            SUB(y, x);
+            ADD(temp, y);
+            reg_m_drop_addr(r_set, y->addr);
+
+            JUMP_i_idx(2);
+            INC(temp);
+            JZERO_i_idx(temp, -1);
+            reg_m_promote(r_set, temp);
+
+            return temp;
             break;
         case cond_IS_N_EQUAL:
-            return NULL;
+            if (y->flags & REG_MODIFIED) {
+                stack_ptr_generate(y->addr);
+                STORE(y, &(r_set->stack_ptr));
+                y->flags &= ~REG_MODIFIED;
+            }
+            reg *temp = oper_get_reg_for_variable(TEMP_ADDR_3).r;
+            oper_reg_swap(temp, x);
+            SUB(temp, y);
+            SUB(y, x);
+            ADD(temp, y);
+            reg_m_drop_addr(r_set, y->addr);
+            reg_m_promote(r_set, temp);
+
+            return temp;
             break;
         case cond_LESS:
             if (y->flags & REG_MODIFIED) {
@@ -71,11 +101,31 @@ reg * cond_val_from_vals(val src_1, val src_2, expr_type type) {
 
             return x;
             break;
-        case cond_LESS_EQ:
-            return NULL;
+        case cond_LESS_EQ: // x <= y
+            if (y->flags & REG_MODIFIED) {
+                stack_ptr_generate(y->addr);
+                STORE(y, &(r_set->stack_ptr));
+                y->flags &= ~REG_MODIFIED;
+            }
+            INC(y);
+            SUB(y, x);
+            y->addr = TEMP_ADDR_2;
+            reg_m_promote(r_set, y->addr);
+
+            return y;
             break;
-        case cond_GREATER_EQ:
-            return NULL;
+        case cond_GREATER_EQ: // x >= y
+            if (x->flags & REG_MODIFIED) {
+                stack_ptr_generate(x->addr);
+                STORE(x, &(r_set->stack_ptr));
+                x->flags &= ~REG_MODIFIED;
+            }
+            INC(x);
+            SUB(x, y);
+            x->addr = TEMP_ADDR_1;
+            reg_m_promote(r_set, x->addr);
+
+            return x;
             break;
         default:
             fprintf(stderr, "[COND]: Wrong value of cond_type!\n");
