b94c8d651cba1b029a07c10ad97516474232a657
pan_orka <ltgaming.reterri@gmail.com>
2021-01-10 23:47:41 +0100

    full if

--- instruction_graph/if.c
+++ instruction_graph/if.c
@@ -104,10 +104,23 @@ void eval_ENDIF(i_graph **i_current) {
         // ENDING ELSE
         oper_regs_store_drop();
         reg_set *r_set = get_reg_set();
-        stack_ptr_clear();
         *(i_else.reserved_jmp) = ((int64_t)asm_get_i_num() - (int64_t)i_else.i_num) + 1;
+        stack_ptr_clear();
     } else if (i_level_pop_branch_eval(false).type == i_IF) {
         i_level i_if = i_level_pop_branch_eval(true);
+        oper_regs_store_drop();
+
+        reg_set *r_set = get_reg_set();
+        reg_m_apply_snapshot(r_set, i_if.r_snap);
+        stack_ptr_clear();
+
+        JUMP();
+        i_level_add_branch_eval(i_ENDIF);
+        i_level i_endif = i_level_pop_branch_eval(true);
+        *(i_if.reserved_jmp) = ((int64_t)i_endif.i_num - (int64_t)i_if.i_num) + 1;
+        oper_regs_store_drop();
+        *(i_endif.reserved_jmp) = ((int64_t)asm_get_i_num() - (int64_t)i_endif.i_num) + 1;
+        stack_ptr_clear();
     } else {
         fprintf(stderr, "[EVAL_ENDIF]: No matching type of branch!\n");
     }
