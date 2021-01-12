608eb458173005ac9833d4571c9663a9f69d8d62
pan_orka <ltgaming.reterri@gmail.com>
2021-01-12 20:53:51 +0100

    proper addr handle

--- instruction_graph/io.c
+++ instruction_graph/io.c
@@ -148,7 +148,15 @@ void eval_WRITE(i_graph **i_current) {
     expression_t const * const expr = (*i_current)->payload;
     reg_set *r_set = get_reg_set();
 
-    if (!(expr->mask & LEFT_SYM1_NUM)) {
+    const bool left_sym_1_addr = expr->addr_mask & LEFT_SYM1_ADDR;
+    if (left_sym_1_addr) {
+        stack_ptr_generate(expr->var_1[1].addr);
+        reg_allocator reg_alloc = reg_m_get(r_set, expr->var_1[1].addr, false);
+        if (reg_alloc.was_allocated && (reg_alloc.r->flags & REG_MODIFIED)) {
+            STORE(reg_alloc.r, &(r_set->stack_ptr));
+            reg_alloc.r->flags &= ~REG_MODIFIED;
+        }
+    } else if (!(expr->mask & LEFT_SYM1_NUM)) {
         if ((expr->var_1[1].var->flags & SYMBOL_IS_ARRAY) && !(expr->mask & LEFT_SYM2_NUM)) {
             const bool left_sym_2_addr = expr->addr_mask & LEFT_SYM2_ADDR;
             const bool left_sym_2_const = !left_sym_2_addr && (expr->var_2[1].var->flags & SYMBOL_IS_CONSTANT);
--- instruction_graph/std_oper/std_oper.c
+++ instruction_graph/std_oper/std_oper.c
@@ -399,7 +399,15 @@ val oper_get_assign_val_1(expression_t const * const expr) {
         .is_reg = true
     };
 
-    if (!(expr->mask & LEFT_SYM1_NUM)) {
+    const bool left_sym_1_addr = expr->addr_mask & LEFT_SYM1_ADDR;
+    if (left_sym_1_addr) {
+        reg_allocator var = oper_get_reg_for_variable(expr->var_1[1].addr);
+        if (!var.was_allocated) {
+            oper_load_variable_to_reg(var.r, expr->var_1[1].addr);
+        }
+
+        assign_val.reg = var.r;
+    } else if (!(expr->mask & LEFT_SYM1_NUM)) {
         if ((expr->var_1[1].var->flags & SYMBOL_IS_ARRAY) && !(expr->mask & LEFT_SYM2_NUM)) {
             const bool left_sym_2_addr = expr->addr_mask & LEFT_SYM2_ADDR;
             const bool left_sym_2_const = !left_sym_2_addr && (expr->var_2[1].var->flags & SYMBOL_IS_CONSTANT);
@@ -449,8 +457,7 @@ val oper_get_assign_val_1(expression_t const * const expr) {
                 assign_val.reg = var.r;
             }
         } else {
-            const bool left_sym_1_addr = expr->addr_mask & LEFT_SYM1_ADDR;
-            const bool left_sym_1_const = !left_sym_1_addr && (expr->var_1[1].var->flags & SYMBOL_IS_CONSTANT);
+            const bool left_sym_1_const = expr->var_1[1].var->flags & SYMBOL_IS_CONSTANT;
 
             if (left_sym_1_const) {
                 assign_val.is_reg = false;
@@ -473,14 +480,6 @@ val oper_get_assign_val_1(expression_t const * const expr) {
                 } else {
                     mpz_init_set(assign_val.constant, expr->var_1[1].var->consts.value);
                 }
-            } else if (left_sym_1_addr) {
-                reg_allocator var = oper_get_reg_for_variable(expr->var_1[1].addr);
-
-                if (!var.was_allocated) {
-                    oper_load_variable_to_reg(var.r, expr->var_1[1].addr);
-                }
-
-                assign_val.reg = var.r;
             } else {
                 assign_val.reg = load_sym_reg_from_num(expr->var_1[1].var, expr->var_2[1].num);
             }
@@ -500,7 +499,15 @@ val oper_get_assign_val_2(expression_t const * const expr) {
         .is_reg = true
     };
 
-    if (!(expr->mask & RIGHT_SYM1_NUM)) {
+    const bool right_sym_1_addr = expr->addr_mask & RIGHT_SYM1_ADDR;
+    if (right_sym_1_addr) {
+        reg_allocator var = oper_get_reg_for_variable(expr->var_1[2].addr);
+        if (!var.was_allocated) {
+            oper_load_variable_to_reg(var.r, expr->var_1[2].addr);
+        }
+
+        assign_val.reg = var.r;
+    } else if (!(expr->mask & RIGHT_SYM1_NUM)) {
         if ((expr->var_1[2].var->flags & SYMBOL_IS_ARRAY) && !(expr->mask & RIGHT_SYM2_NUM)) {
             const bool right_sym_2_addr = expr->addr_mask & RIGHT_SYM2_ADDR;
             const bool right_sym_2_const = !right_sym_2_addr && (expr->var_2[2].var->flags & SYMBOL_IS_CONSTANT);
@@ -550,8 +557,7 @@ val oper_get_assign_val_2(expression_t const * const expr) {
                 assign_val.reg = var.r;
             }
         } else {
-            const bool right_sym_1_addr = expr->addr_mask & RIGHT_SYM1_ADDR;
-            const bool right_sym_1_const = !right_sym_1_addr && expr->var_1[2].var->flags & SYMBOL_IS_CONSTANT;
+            const bool right_sym_1_const = expr->var_1[2].var->flags & SYMBOL_IS_CONSTANT;
 
             if (right_sym_1_const) {
                 assign_val.is_reg = false;
@@ -574,14 +580,6 @@ val oper_get_assign_val_2(expression_t const * const expr) {
                 } else {
                     mpz_init_set(assign_val.constant, expr->var_1[2].var->consts.value);
                 }
-            } else if (right_sym_1_addr) {
-                reg_allocator var = oper_get_reg_for_variable(expr->var_1[2].addr);
-
-                if (!var.was_allocated) {
-                    oper_load_variable_to_reg(var.r, expr->var_1[2].addr);
-                }
-
-                assign_val.reg = var.r;
             } else {
                 assign_val.reg = load_sym_reg_from_num(expr->var_1[2].var, expr->var_2[2].num);
             }
