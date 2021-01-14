8de7df740e986a0aa34c4fae6aa87f3f01374e31
pan_orka <ltgaming.reterri@gmail.com>
2021-01-14 17:01:51 +0100

    end of repeat_until

--- instruction_graph/repeat_until.c
+++ instruction_graph/repeat_until.c
@@ -41,6 +41,32 @@ void add_UNTIL(expression_t *expr) {
     exit(EXIT_FAILURE);
 }
 
+static expr_type repeat_until_inverse_cond(expr_type type) {
+    switch (type) {
+        case cond_IS_EQUAL:
+            return cond_IS_N_EQUAL;
+            break;
+        case cond_IS_N_EQUAL:
+            return cond_IS_EQUAL;
+            break;
+        case cond_LESS:
+            return cond_GREATER_EQ;
+            break;
+        case cond_GREATER:
+            return cond_LESS_EQ;
+            break;
+        case cond_LESS_EQ:
+            return cond_GREATER;
+            break;
+        case cond_GREATER_EQ:
+            return cond_LESS;
+            break;
+        default:
+            fprintf(stderr, "[COND_inv]: Wrong value of cond_type!\n");
+            exit(EXIT_FAILURE);
+    }
+}
+
 void eval_UNTIL(i_graph **i_current) {
     i_level i_repeat = i_level_pop_branch_eval(true);
     expression_t const * const expr = (*i_current)->payload;
@@ -63,9 +89,18 @@ void eval_UNTIL(i_graph **i_current) {
             exit(EXIT_FAILURE);
         }
     } else {
-        reg *x = cond_val_from_vals(assign_val_1, assign_val_2, expr->type);
+        const expr_type cond_type = repeat_until_inverse_cond(expr->type);
+        reg *x = cond_val_from_vals(assign_val_1, assign_val_2, cond_type);
         oper_regs_store_drop();
 
-        
+        if (cond_type != cond_IS_EQUAL) {
+            JZERO(x); // compare
+        } else {
+            JZERO_i_idx(x, 2);
+            JUMP();
+        }
+        i_level_set_reserved_jump(i_repeat.reserved_jmp_idx,
+                                  (int64_t)i_repeat.i_num + 1 - (int64_t)asm_get_i_num());
+        stack_ptr_clear();
     }
 }
